0c87011a930f8f37e1d5a43a4206d04cf0649d18
Joana Grave <joana_grave@hotmail.com>
2023-02-28 11:35:48 -0500

    initial commit

---
+++ queue.js
@@ -0,0 +1,73 @@
+/**
+ * Class to represent a queue using an array to store the queued items.
+ * Follows a FIFO (First In First Out) order where new items are added to the
+ * back and items are removed from the front.
+ */
+class Queue {
+  constructor() {
+    this.items = [];
+  }
+
+  /**
+   * Adds a new given item to the back of this queue.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @param {any} item The new item to add to the back.
+   * @returns {number} The new size of this queue.
+   */
+  enqueue(item) {
+    this.items.push(item);
+    return this.size();
+  }
+
+  /**
+   * Removes and returns the first item of this queue.
+   * - Time: O(n) linear, due to having to shift all the remaining items to
+   *    the left after removing first elem.
+   * - Space: O(1) constant.
+   * @returns {any} The first item or undefined if empty.
+   */
+  dequeue() {
+    return this.items.shift();
+  }
+
+  /**
+   * Retrieves the first item without removing it.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @returns {any} The first item or undefined if empty.
+   */
+  front() {
+    return this.items[0];
+  }
+
+  /**
+   * Returns whether or not this queue is empty.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @returns {boolean}
+   */
+  isEmpty() {
+    return this.size() === 0;
+  }
+
+  /**
+   * Retrieves the size of this queue.
+   * - Time: O(1) constant.
+   * - Space: O(1) constant.
+   * @returns {number} The length.
+   */
+  size() {
+    return this.items.length;
+  }
+}
+
+/* Rebuild the above class using a linked list instead of an array. */
+
+const myqueue = new Queue();
+console.log(myqueue.enqueue(42));
+console.log("front: ", myqueue.front());
+console.log(myqueue.enqueue(13));
+console.log(myqueue.dequeue());
+console.log("front: ",myqueue.front());
+// console.log(myqueue);
